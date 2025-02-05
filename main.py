d93d2038234dcd1ea0f0f562b306f0c8baddf9ce
Ольга Костенко <you@example.com>
2025-02-05 20:08:30 +0500

    Внесены изменения

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
 # Импорт встроенной библиотеки для работы веб-сервера
 from http.server import BaseHTTPRequestHandler, HTTPServer
-#import time
+from config import file_contact_html
 
 # Для начала определим настройки запуска
-hostName = "localhost" # Адрес для доступа по сети
-serverPort = 8080 # Порт для доступа по сети
+hostName = "localhost"  # Адрес для доступа по сети
+serverPort = 8080  # Порт для доступа по сети
+
 
 class MyServer(BaseHTTPRequestHandler):
     """
         Специальный класс, который отвечает за
         обработку входящих запросов от клиентов
     """
+
     def do_GET(self):
-        """ Метод для обработки входящих GET-запросов """
-        self.send_response(200) # Отправка кода ответа
-        self.send_header("Content-type", "text/html") # Отправка типа данных, который будет передаваться
-        self.end_headers() # Завершение формирования заголовков ответа
-        with open("pages/home.html", "r", encoding="utf-8") as file:
-            page = file.read()
-        self.wfile.write(bytes(page, "utf-8")) # Тело ответа
+        if self.path == "/" or self.path == "/home.html":
+            file_to_serve = file_contact_html
+        elif self.path == "/contacts.html":
+            file_to_serve = "contacts.html"
+        elif self.path == "/category.html":
+            file_to_serve = "category.html"
+        elif self.path == "/catalog.html":
+            file_to_serve = "catalog.html"
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(bytes("404 Not Found", "utf-8"))
+            return
+
+        try:
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            with open(file_to_serve, encoding="utf-8") as file:
+                page = file.read()
+            self.wfile.write(bytes(page, "utf-8"))
+        except FileNotFoundError:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(bytes("404 Not Found", "utf-8"))
+
 
 if __name__ == "__main__":
     # Инициализация веб-сервера, который будет по заданным параметрах в сети
